2e7b21fa020a993751eb693c8aaace7b8698a939
SilversKnit <rahultokill@gmail.com>
2023-02-08 14:25:38 +0530

    PDFreport and Srcnshot Added in ExtentReport

--- src/test/java/com/Step_Definition/StepDefinition.java
+++ src/test/java/com/Step_Definition/StepDefinition.java
@@ -1,12 +1,16 @@
 package com.Step_Definition;
 
 import java.io.IOException;
+import java.sql.DriverManager;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import com.Base_Class.BaseClass;
 import com.POManger.Class.Manager;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.Status;
@@ -31,6 +35,15 @@ public class StepDefinition extends BaseClass{
 		if(s.isFailed()) {
 			sshot("failed.png");
 		}
+	}
+	
+	@AfterStep
+	public void attachScreenshot(Scenario s) {
+		if(s.isFailed()) {
+			byte[] TakenSShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+			s.attach(TakenSShot, "image/png", "err scrn");
+		}
+		
 	}
 	
 	
